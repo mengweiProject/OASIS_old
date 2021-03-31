8fe18108436bfb17e69c60c07cb627e507eae608
mengwei1992 <13395267298@163.com>
2021-03-31 17:29:55 +0800

    #OASIS-011#有参数装饰器示例

---
+++ BaseGrammar/孟威/decoratorExp.py
@@ -0,0 +1,22 @@
+'''
+@Project ：OASIS 
+@Author  ：孟威
+@Date    ：2021/3/31 11:03 
+'''
+
+
+def decorator(func):
+    def run(username, pwd):
+        print("验证...")
+        func(username, pwd)
+        return 123
+    return run
+
+
+@decorator
+def login(username, pwd):
+    print("login...", username, pwd)
+    return 1
+
+if __name__ == '__main__':
+    login(212312, 23452345)
