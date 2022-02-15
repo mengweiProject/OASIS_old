d51b542504e076d76b0112338a28628e0f4bc856
mengwei1992 <13395267298@163.com>
2022-02-15 09:11:06 +0800

    #OASIS-018#commonGrammar

--- DjangoOASIS/blog/urls.py
+++ DjangoOASIS/blog/urls.py
@@ -5,5 +5,7 @@ from . import views
 urlpatterns = [
     path("index", views.index, name="index"),
     path("service", views.serviceList, name="serviceList"),
-    path("testJson", views.testJson, name="testJson")
+    path("testJson", views.testJson, name="testJson"),
+    path("set_cookies_t", views.set_cookies_t, name="set_cookies_t"),
+    path("get_cookies_t", views.get_cookies_t, name="get_cookies_t"),
 ]
--- DjangoOASIS/blog/views.py
+++ DjangoOASIS/blog/views.py
@@ -17,3 +17,15 @@ def serviceList(request):
 def testJson(request):
     infoJson = {"name": "威震天", "age": "999", "wars": ["大战擎天柱", "手撕大黄蜂", "脚踢救护车"]}
     return HttpResponse(json.dumps(infoJson))
+
+
+def set_cookies_t(request):
+    res = HttpResponse('success')
+    res.set_cookie('my_cookie', value='xiaohuang', max_age=100)
+    return res
+
+
+def get_cookies_t(request):
+    cookie = request.COOKIES.get('my_cookie', None)
+    print(cookie)
+    return HttpResponse('success get cookies')
