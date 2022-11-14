e9c204be829b7957a4a8a53d2c91a1d1f31a6020
mengwei1992 <13395267298@163.com>
2022-11-14 08:20:09 +0800

    #OASIS-046#是否快乐数

--- BaseGrammar/孟威/testCase.py
+++ BaseGrammar/孟威/testCase.py
@@ -9,16 +9,36 @@ from time import sleep
 
 from tools import calcTimeCost, calcTimeCostParams
 
-@calcTimeCost
-def test():
-    sleep(3)
+# @calcTimeCost
+# def test():
+#     sleep(3)
+#
+#
+# @calcTimeCostParams
+# def testParam(n, m):
+#     sleep(n)
+#
+#
+# if __name__ == '__main__':
+#     # test()
+#     testParam(3, 3)
+
+# from functools import reduce
+#
+# class Solution:
+#     def singleNumber(self, nums):
+#         return reduce(lambda x, y: x ^ y, nums)
+#
+# if __name__ == '__main__':
+#     l = [i for i in range(101)]
+#     for i in l:
+#         print(i ^ (i + 1))
+#     print(reduce(lambda x, y: x ^ y, l))
 
 
-@calcTimeCostParams
-def testParam(n, m):
-    sleep(n)
+# print(list(str(123)))
 
-
-if __name__ == '__main__':
-    # test()
-    testParam(3, 3)
+class Person:
+    name = 'Human'
+    def run(self):
+        print(Person.name)
