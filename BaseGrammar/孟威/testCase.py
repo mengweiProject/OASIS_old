cfb16ddc3c31e68f11abd7485dca256b48262fdd
mengwei1992 <13395267298@163.com>
2022-12-02 10:05:28 +0800

    #OASIS-046#dailyCodes

--- BaseGrammar/孟威/testCase.py
+++ BaseGrammar/孟威/testCase.py
@@ -38,7 +38,58 @@ from tools import calcTimeCost, calcTimeCostParams
 
 # print(list(str(123)))
 
-class Person:
-    name = 'Human'
-    def run(self):
-        print(Person.name)
+# class Person:
+#     name = 'Human'
+#     def run(self):
+#         print(Person.name)
+
+
+if __name__ == '__main__':
+    # a = 111
+    # b = a
+    # print(id(a))
+    # print(id(b))
+    # print(a is b)
+    # s1 = 'asdgjkajiogwerioogeabj'
+    # s2 = s1
+    # print(id(s1))
+    # print(id(s2))
+    # print(s1 is s2)
+    # l1 = [1, 2, 3, 6, 7, [9, 0]]
+    # l2 = l1
+    # print(id(l1))
+    # print(id(l2))
+    # print(l1 is l2)
+
+    l1 = [1, 2, 3, 4, [5, 6]]
+    l2 = l1.copy()
+    print(id(l1))
+    print(id(l2))
+    print(id(l1[4]))
+    print(id(l2[4]))
+    print(l1 is l2)
+
+    import copy
+    l3 = [1, 2, 3, 4, [5, 6]]
+    l4 = copy.deepcopy(l3)
+    print(id(l3))
+    print(id(l4))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
