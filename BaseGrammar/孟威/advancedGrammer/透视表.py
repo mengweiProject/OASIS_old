802e9a57bc8672920331b882917e61c7acf12bc5
mengwei <13395267298@163.com>
2023-05-08 15:03:18 +0800

    #OASIS-033#daily-code001

---
+++ BaseGrammar/孟威/advancedGrammer/透视表.py
@@ -0,0 +1,7 @@
+
+import datedays
+import pandas as pd
+
+print(datedays.getstartend(start_date='2022-08-01', end_date='2022-08-07', list_=True))
+
+pd.pivot()
---
+++ Spider/宰相刘罗锅/spider07.py
@@ -0,0 +1,196 @@
+
+
+
+
+
+
+import requests
+from time import sleep
+
+
+
+# cont = b""
+# for i in range(1315):
+for i in range(1310):
+    with open(f'C:\\Users\\13395\\Desktop\\videos\\宰相刘罗锅\\025.mp4', 'ab') as f:
+        url = f'https://wolongzywcdn2.com:65/20220508/2WV5G7Jq/0{i}.ts'
+        # sleep(0.05)
+        print(url)
+        res = requests.get(url).content
+        # cont += res
+        f.write(res)
+
+
+for i in range(1310):
+    with open(f'C:\\Users\\13395\\Desktop\\videos\\宰相刘罗锅\\026.mp4', 'ab') as f:
+        url = f'https://wolongzywcdn2.com:65/20220508/T0g3TQDX/0{i}.ts'
+        # sleep(0.05)
+        print(url)
+        res = requests.get(url).content
+        # cont += res
+        f.write(res)
+
+for i in range(1310):
+    with open(f'C:\\Users\\13395\\Desktop\\videos\\宰相刘罗锅\\027.mp4', 'ab') as f:
+        url = f'https://wolongzywcdn2.com:65/20220508/2DxrBuvI/0{i}.ts'
+        # sleep(0.05)
+        print(url)
+        res = requests.get(url).content
+        # cont += res
+        f.write(res)
+
+for i in range(1310):
+    with open(f'C:\\Users\\13395\\Desktop\\videos\\宰相刘罗锅\\028.mp4', 'ab') as f:
+        url = f'https://wolongzywcdn2.com:65/20220508/FqJ1XCGO/0{i}.ts'
+        # sleep(0.05)
+        print(url)
+        res = requests.get(url).content
+        # cont += res
+        f.write(res)
+
+for i in range(1310):
+    with open(f'C:\\Users\\13395\\Desktop\\videos\\宰相刘罗锅\\029.mp4', 'ab') as f:
+        url = f'https://wolongzywcdn2.com:65/20220508/bhc6whoW/0{i}.ts'
+        # sleep(0.05)
+        print(url)
+        res = requests.get(url).content
+        # cont += res
+        f.write(res)
+
+for i in range(1310):
+    with open(f'C:\\Users\\13395\\Desktop\\videos\\宰相刘罗锅\\030.mp4', 'ab') as f:
+        url = f'https://wolongzywcdn2.com:65/20220508/tIkfthSX/0{i}.ts'
+        # sleep(0.05)
+        print(url)
+        res = requests.get(url).content
+        # cont += res
+        f.write(res)
+
+
+
+
+for i in range(1310):
+    with open(f'C:\\Users\\13395\\Desktop\\videos\\宰相刘罗锅\\031.mp4', 'ab') as f:
+        url = f'https://wolongzywcdn2.com:65/20220508/fonPesIx/0{i}.ts'
+        # sleep(0.05)
+        print(url)
+        res = requests.get(url).content
+        # cont += res
+        f.write(res)
+
+for i in range(1310):
+    with open(f'C:\\Users\\13395\\Desktop\\videos\\宰相刘罗锅\\032.mp4', 'ab') as f:
+        url = f'https://wolongzywcdn2.com:65/20220508/g2GMTKoa/0{i}.ts'
+        # sleep(0.05)
+        print(url)
+        res = requests.get(url).content
+        # cont += res
+        f.write(res)
+
+for i in range(1310):
+    with open(f'C:\\Users\\13395\\Desktop\\videos\\宰相刘罗锅\\033.mp4', 'ab') as f:
+        url = f'https://wolongzywcdn2.com:65/20220508/xKZWlnVw/0{i}.ts'
+        # sleep(0.05)
+        print(url)
+        res = requests.get(url).content
+        # cont += res
+        f.write(res)
+
+for i in range(1310):
+    with open(f'C:\\Users\\13395\\Desktop\\videos\\宰相刘罗锅\\034.mp4', 'ab') as f:
+        url = f'https://wolongzywcdn2.com:65/20220508/6QmKBsun/0{i}.ts'
+        # sleep(0.05)
+        print(url)
+        res = requests.get(url).content
+        # cont += res
+        f.write(res)
+
+for i in range(1310):
+    with open(f'C:\\Users\\13395\\Desktop\\videos\\宰相刘罗锅\\035.mp4', 'ab') as f:
+        url = f'https://wolongzywcdn2.com:65/20220508/tQo6GudH/0{i}.ts'
+        # sleep(0.05)
+        print(url)
+        res = requests.get(url).content
+        # cont += res
+        f.write(res)
+
+for i in range(1310):
+    with open(f'C:\\Users\\13395\\Desktop\\videos\\宰相刘罗锅\\036.mp4', 'ab') as f:
+        url = f'https://wolongzywcdn2.com:65/20220508/SP3XHvDn/0{i}.ts'
+        # sleep(0.05)
+        print(url)
+        res = requests.get(url).content
+        # cont += res
+        f.write(res)
+
+for i in range(1310):
+    with open(f'C:\\Users\\13395\\Desktop\\videos\\宰相刘罗锅\\037.mp4', 'ab') as f:
+        url = f'https://wolongzywcdn2.com:65/20220508/oDuLajti/0{i}.ts'
+        # sleep(0.05)
+        print(url)
+        res = requests.get(url).content
+        # cont += res
+        f.write(res)
+
+for i in range(1310):
+    with open(f'C:\\Users\\13395\\Desktop\\videos\\宰相刘罗锅\\038.mp4', 'ab') as f:
+        url = f'https://wolongzywcdn2.com:65/20220508/LwwgCdzl/0{i}.ts'
+        # sleep(0.05)
+        print(url)
+        res = requests.get(url).content
+        # cont += res
+        f.write(res)
+
+for i in range(1310):
+    with open(f'C:\\Users\\13395\\Desktop\\videos\\宰相刘罗锅\\039.mp4', 'ab') as f:
+        url = f'https://wolongzywcdn2.com:65/20220508/dpVBMdio/0{i}.ts'
+        # sleep(0.05)
+        print(url)
+        res = requests.get(url).content
+        # cont += res
+        f.write(res)
+
+for i in range(1310):
+    with open(f'C:\\Users\\13395\\Desktop\\videos\\宰相刘罗锅\\040.mp4', 'ab') as f:
+        url = f'https://wolongzywcdn2.com:65/20220508/jcttbWb5/0{i}.ts'
+        # sleep(0.05)
+        print(url)
+        res = requests.get(url).content
+        # cont += res
+        f.write(res)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
