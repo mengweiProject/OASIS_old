d0ffc0c451793ac7c7593474aaf8254b3d576b87
mengwei <13395267298@163.com>
2023-05-08 15:00:05 +0800

    #OASIS-033#daily-code001

--- BaseGrammar/孟威/advancedGrammer/leetcodeExp.py
+++ BaseGrammar/孟威/advancedGrammer/leetcodeExp.py
@@ -13,14 +13,38 @@ def climbStairs(self, n: int) -> int:
 
 '''
 
+import timeit
+import pysnooper
 
 
-def maxSubArray(nums):
-    for i in range(1, len(nums)):
-        nums[i] = nums[i] + max(nums[i - 1], 0)
-        print(nums)
-    return max(nums)
+# def maxSubArray(nums):
+#     for i in range(1, len(nums)):
+#         nums[i] = nums[i] + max(nums[i - 1], 0)
+#         print(nums)
+#     return max(nums)
+#
+# nums = [-2,1,-3,4,-1,2,1,-5,4]
+# print(nums)
+# print(maxSubArray(nums))
 
-nums = [-2,1,-3,4,-1,2,1,-5,4]
-print(nums)
-print(maxSubArray(nums))
+
+@pysnooper.snoop()
+def countElements(nums):
+    print(min(nums))
+    print(max(nums))
+    print(min(nums) < num < max(nums) for num in nums)
+    for num in nums:
+        print(min(nums) < num < max(nums))
+    print(sum(i > 10 for i in range(20)))
+    return sum(min(nums) < num < max(nums) for num in nums)
+
+def calc_s():
+    n = 0
+    for i in range(1000000):
+        n += i
+
+
+nums = [11,7,2,15]
+countElements(nums)
+# print(timeit.timeit('calc_s()', 'from __main__ import calc_s', number=2))
+
